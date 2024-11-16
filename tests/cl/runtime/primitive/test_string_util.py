0e9e7d763c0d7fc31cd65ddc23f331e92328e08f
Alexander Sokol <sokol@compatibl.com>
2024-11-15 23:26:03 -0500

    Add digest method to StringUtil.

--- tests/cl/runtime/primitive/test_string_util.py
+++ tests/cl/runtime/primitive/test_string_util.py
@@ -14,6 +14,7 @@
 
 import pytest
 from cl.runtime.primitive.string_util import StringUtil
+from cl.runtime.testing.regression_guard import RegressionGuard
 
 
 def test_is_empty():
@@ -21,7 +22,37 @@ def test_is_empty():
     assert StringUtil.is_empty(None)
     assert StringUtil.is_empty("")
     assert not StringUtil.is_empty("abc")
+    
+    
+def test_digest():
+    """Test EntryKey.create_key method."""
 
+    guard = RegressionGuard()
+
+    # Record type
+    locale = "en-GB"
+
+    guard.write("Check short text without params with and without data, hash should be different")
+    guard.write(StringUtil.digest(text="Sample Text"))
+    guard.write(StringUtil.digest(text="Sample Text", hash_params=("Sample Data",)))
+
+    guard.write("Check short text with params with and without data, hash should be different")
+    guard.write(StringUtil.digest(text="Sample Text", text_params=(locale,)))
+    guard.write(StringUtil.digest(text="Sample Text", text_params=(locale,), hash_params=("Sample Data",)))
+
+    guard.write("Check long text with and without data, hash should be different")
+    guard.write(StringUtil.digest(text=" ".join(20 * ["Long Text"]), text_params=(locale,)))
+    guard.write(StringUtil.digest(text=" ".join(20 * ["Long Text"]), text_params=(locale,), hash_params=("Sample Data",)))
+
+    guard.write("Check multiline text with and without data, hash should be different")
+    guard.write(StringUtil.digest(text="Multiline\nText", text_params=(locale,)))
+    guard.write(StringUtil.digest(text="Multiline\nText", text_params=(locale,), hash_params=("Sample Data",)))
+
+    guard.write("The same inputs one more time, hash should be the same")
+    guard.write(StringUtil.digest(text="Multiline\nText", text_params=(locale,), hash_params=("Sample Data",)))
+
+    # Verify
+    guard.verify_all()
 
 if __name__ == "__main__":
     pytest.main([__file__])
